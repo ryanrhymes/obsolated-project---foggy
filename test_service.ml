a70ed7505519e06e4dce86f41a024a75b01ff632
Liang Wang <ryanrhymes@gmail.com>
2015-07-14 22:55:33 +0100

    coding ...

--- test_service.ml
+++ test_service.ml
@@ -9,30 +9,3 @@ open Service
 
 let _ = print_endline "test Broker ..."
 let _ = Service.pull "http://www.google.com"
-
-type both = | Int of int
-	    | Float of float
-
-let test x = 
-  match x with
-  | Int y -> Int (y + 1)
-  | Float y -> Float (y +. 2.)
-
-
-let a = test (Int 5)
-let b = test (Float 6.)
-
-let prt x =  match x with 
-  | Int x -> print_endline (string_of_int x)
-  | Float x -> print_endline (string_of_float x)
-
-let prr = function
-  | Int x -> print_endline (string_of_int x)
-  | Float x -> print_endline (string_of_float x)
-
-let _ = prr a
-let _ = prr b
-
-(**
-let _ = print_endline (string_of_int (both 5))
-**)
