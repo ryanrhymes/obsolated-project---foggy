cd0643da899515bc63f482084eed793196bd32a7
Liang Wang <ryanrhymes@gmail.com>
2015-07-14 20:53:25 +0100

    coding ...

---
+++ service.ml
@@ -0,0 +1,22 @@
+(**
+   Service module for service caching
+   This module deals with service migration and instantiation.
+
+   Liang Wang @ Computer Lab, Cambridge University, UK
+   2015.07.14
+**)
+
+
+module Service = struct
+
+  let pull = 0
+
+  let push = 0
+
+  let start = 0
+
+  let stop = 0
+
+  let pause = 0
+
+end
